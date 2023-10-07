f1b0553d08e51eb709494c2a9150f519caf4214f
raquel <rachel050479@gmail.com>
2023-10-07 09:46:55 +0200

    added changes

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ function searchShows(event) {
   event.preventDefault();
   let inputValue = searchInput.value;
   let url = `https://api.tvmaze.com/search/shows?q=${inputValue}`;
-
+  //petición a la Api y procesa la respuesta JSON para extraer la información de la serie
   fetch(url)
     .then((response) => response.json())
     .then((dataApi) => {
@@ -41,7 +41,6 @@ function searchShows(event) {
           id: item.show.id,
           name: item.show.name,
           photo: urlImage,
-          schedule: item.show.schedule,
         };
 
         //agregar la serie a la lista
@@ -52,48 +51,32 @@ function searchShows(event) {
     });
 }
 
-//función para crear una serie y nos devuelve el objeto(tarjeta de serie)
+//función que recibe un objeto de serie y crea el HTM para mostrarlo en la lista de series, si está ya en la lista de favoritas le añade la clase favorite
 function renderShow(oneShow) {
   let objectShow = '';
-  let hasSchedule = oneShow.schedule.days; // [day1, day2, day3]
   const isFav = favShowsList.some((favShow) => favShow.id === oneShow.id);
-  //const hasSchedule = oneShow.includes(oneShow.length);
-
-  //hasSchedule ? vacío : oneShow.schedule.days;
   if (isFav) {
     objectShow = `<li class="card js-card favorite" id="${oneShow.id}">
       <h3 class="show-name">${oneShow.name}</h3>
-      <p class="show-schedule">${
-        hasSchedule.length === 0 ? 'No schedules' : hasSchedule
-      }</p>
-      <img class="card-img" src="${oneShow.photo}" title=${oneShow.name} alt="${
-      oneShow.name
-    }"/>
+      <img class="card-img" src="${oneShow.photo}" title=${oneShow.name} alt="${oneShow.name}"/>
       </li>`;
   } else {
     objectShow = `<li class="card js-card" id="${oneShow.id}">
     <h3 class="show-name">${oneShow.name}</h3>
-    <p class="show-schedule">${oneShow.schedule.days}</p>
     <img class="card-img" src="${oneShow.photo}" title=${oneShow.name} alt="${oneShow.name}"/>
     </li>`;
   }
-
+  //devuelve la tarjeta de la serie
   return objectShow;
 }
 
-//función para pintar la lista de series
+//función para pintar la lista de series según el input que recibe
 function renderShowList(shows) {
-  showList.innerHTML = '';
+  showList.innerHTML = ''; //limpia el contenido actual de la lista
   for (const item of shows) {
     showList.innerHTML += renderShow(item);
   }
   addEventsToShows();
-}
-
-//función para pintar la lista de favoritas
-function renderFavList(favShows) {
-  const favListHTML = favShows.map((item) => renderShow(item, true)).join('');
-  favList.innerHTML = favListHTML;
 }
 
 //FUNCIÓN PARA AÑADIR UNA SERIE A LA LISTA DE FAVORITAS
@@ -117,9 +100,32 @@ function addToFav(event) {
     favShowsList.splice(indexFav, 1);
     event.currentTarget.classList.remove('favorite'); //quita la clase favorite para cambiar estilos
   }
-  console.log(`Nombre: ${foundShow.name}`);
-  renderFavList(favShowsList);
+
+  renderFavList(favShowsList); //actualiza la lista de favoritas y la guarda
   localStorage.setItem('favShowsList', JSON.stringify(favShowsList));
+}
+
+//función para pintar la lista de favoritas
+function renderFavList(favShows) {
+  const favListHTML = favShows
+    .map((item) => {
+      return `
+      <li class="card js-card favorite" id="${item.id}">
+        <span class="x-fav" id="remove${item.id}">X</span> <!-- Botón X -->
+        <h3 class="show-name">${item.name}</h3>
+        <img class="card-img" src="${item.photo}" title="${item.name}" alt="${item.name}"/>
+      </li>
+    `;
+    })
+    .join('');
+  // Agregar la lista de favoritos al elemento HTML favList
+  favList.innerHTML = favListHTML;
+
+  // Asignar eventos de clic a los botones "X" para eliminar programas
+  favShows.forEach((item) => {
+    const removeButton = document.getElementById(`remove${item.id}`);
+    removeButton.addEventListener('click', () => removeFromFav(item.id));
+  });
 }
 
 //función handle que añade una serie favorita a su lista al hacer click
@@ -129,6 +135,23 @@ function addEventsToShows() {
     item.addEventListener('click', addToFav);
   }
   renderFavList(favShowsList);
+}
+
+//eliminar un programa de la lista de favoritos
+function removeFromFav(showId) {
+  // Encuentra el índice del programa en la lista de favoritos
+  const indexFav = favShowsList.findIndex((oneShow) => oneShow.id === showId);
+
+  if (indexFav !== -1) {
+    // Elimina el programa de la lista de favoritos
+    favShowsList.splice(indexFav, 1);
+
+    // Actualiza la lista de favoritos en la interfaz de usuario
+    renderFavList(favShowsList);
+
+    // Actualiza la lista de favoritos en el almacenamiento local
+    localStorage.setItem('favShowsList', JSON.stringify(favShowsList));
+  }
 }
 
 //función que elimina las series favoritas
